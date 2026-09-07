ccc1ed894e5ccaa38d82623dcaa8e0496a4e64e4
Uttkarsh Rishi <uttkarsh.rishi@accordion.com>
2026-09-07 12:24:27 +0000

    abcd

--- macros/create_external_table.sql
+++ macros/create_external_table.sql
@@ -5,7 +5,7 @@ create or replace external table {{ table_name }} (
     FILE_ROW_NUMBER   NUMBER(38,0)       AS (METADATA$FILE_ROW_NUMBER),
     LAST_MODIFIED      TIMESTAMP_NTZ(9)   AS (METADATA$FILE_LAST_MODIFIED)
 )
-location = @BRONZE.ADLS_CAPSTONE_STAGE/
+location = @{{ env_var('DBT_ADLS_STAGE') }}/
 auto_refresh = false
 pattern = '{{ file_pattern }}'
 file_format = BRONZE.JSON_FORMAT
